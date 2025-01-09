10fb3ab1ba006ca4caa331330eca7d103da9baa0
Chris Cooper <cjchrisjcooper@gmail.com>
2025-01-09 08:39:50 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BackToBasicsC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many cans of meat would you like to order:");
+            string cansOrderedAsText = Console.ReadLine();
+            int cansOrdered = Int32.Parse(cansOrderedAsText);
+            Console.WriteLine(cansOrdered);
+            Console.ReadLine();
+
+        }
+    }
+}
