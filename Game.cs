3f872149f6c867087c73a0b4896476b0c2fc083f
Chris Cooper <cjchrisjcooper@gmail.com>
2025-02-03 13:49:59 -0700

    Feat: map class

--- Game.cs
+++ Game.cs
@@ -4,6 +4,6 @@ namespace BackToBasicsC_
     public class Game
     {
         Tower myTower = new Tower();
-
+        Map map = new Map(15, 10);
     }
 }
--- Map.cs
+++ Map.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BackToBasicsC_
+﻿namespace BackToBasicsC_
 {
     internal class Map
     {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
     }
 }
