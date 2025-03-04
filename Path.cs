43dadf06056d2cb613ff8df2af745cea45b50f19
Chris Cooper <cjchrisjcooper@gmail.com>
2025-03-04 01:43:54 -0700

    Feat: Path class implamented

--- Path.cs
+++ Path.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BackToBasicsC_
+﻿namespace BackToBasicsC_
 {
     internal class Path
     {
+        private readonly MapLocation[] _path;
+
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+
+        public MapLocation getLocationAt(int index)
+        {
+            if (index < _path.Length)
+            {
+                return _path[index];
+            }
+            else { return null; }
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,11 +8,24 @@ namespace BackToBasicsC_
         {
             try
             {
-                Point pointOne = new Point(10, 10);
-                Console.WriteLine("Distance to the point is: " + pointOne.DistanceTo(5, 5));
+                Map map = new Map(10, 10);
+                Path path = new Path(
+                    new[]
+                    {
+                    new MapLocation(0,1, map),
+                    new MapLocation(1,1, map),
+                    new MapLocation(2,1, map),
+                    new MapLocation(3,1, map),
+                    new MapLocation(4,1, map),
+                    new MapLocation(5,1, map),
+                    new MapLocation(6,1, map),
+                    new MapLocation(7,1, map),
+                    }
+                    );
 
+                MapLocation thirdLocation = path.getLocationAt(2);
+                Console.WriteLine(thirdLocation.X + "," + thirdLocation.Y);
                 Console.ReadLine();
-
             }
             catch (Exception ae) { Console.WriteLine("We caught an exception: " + ae.Message); }
 
